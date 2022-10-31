895a96000981a8b2be3db7010d46ae2607211ee1
Mariano Chun <marianochun01@gmail.com>
2022-10-31 14:37:55 -0300

    Fix bug addEventListener of null y Separacion eventos en distintos archivos

--- eventosIndex.js
+++ eventosIndex.js
@@ -3,10 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("filtro-excursiones").style.display="none";
 });
 
-document.querySelector('.mostrar-contrasenia #mostrar').addEventListener('click', e => {
-   mostrarContrasenia(e);
-});
-
 function habilitarFiltroPasajes(){
     document.getElementById("filtro-pasajes").style.display="";
     document.getElementById("filtro-paquetes").style.display="none";
@@ -23,17 +19,4 @@ function habilitarFiltroExcursiones(){
     document.getElementById("filtro-pasajes").style.display="none";
     document.getElementById("filtro-paquetes").style.display="none";
     document.getElementById("filtro-excursiones").style.display="";
-}
-
-function mostrarContrasenia(e) {
-    const passwordInput = document.querySelector('#contrasenia');
-    if (e.target.classList.contains('show')) {
-        e.target.classList.remove('show');
-        e.target.textContent = 'Ocultar';
-        passwordInput.type = 'text';
-    } else {
-        e.target.classList.add('show');
-        e.target.textContent = 'Mostrar';
-        passwordInput.type = 'password';
-    }
 }
