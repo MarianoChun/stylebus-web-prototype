e8cd0b7a9a5c0b60eb879d9596cd04b251e2a2cd
Mariano Chun <marianochun01@gmail.com>
2022-10-31 12:57:42 -0300

    Add habilitar/deshabilitar filtros al cambiar uno a otro

--- eventos.js
+++ eventos.js
@@ -1,6 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("filtro-paquetes").style.display="none";
+    document.getElementById("filtro-excursiones").style.display="none";
+});
+
 document.querySelector('.mostrar-contrasenia #mostrar').addEventListener('click', e => {
    mostrarContrasenia(e);
 });
+
+function habilitarFiltroPasajes(){
+    document.getElementById("filtro-pasajes").style.display="";
+    document.getElementById("filtro-paquetes").style.display="none";
+    document.getElementById("filtro-excursiones").style.display="none";
+}
+
+function habilitarFiltroPaquetes(){
+    document.getElementById("filtro-pasajes").style.display="none";
+    document.getElementById("filtro-paquetes").style.display="";
+    document.getElementById("filtro-excursiones").style.display="none";
+}
+
+function habilitarFiltroExcursiones(){
+    document.getElementById("filtro-pasajes").style.display="none";
+    document.getElementById("filtro-paquetes").style.display="none";
+    document.getElementById("filtro-excursiones").style.display="";
+}
 
 function mostrarContrasenia(e) {
     const passwordInput = document.querySelector('#contrasenia');
