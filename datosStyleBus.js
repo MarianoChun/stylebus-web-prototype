1b4a3305942ffff41cec698a4ab75afd29abba64
Mariano Chun <marianochun01@gmail.com>
2022-11-04 10:08:04 -0300

    Rename excursiones.js en DatosStyleBus.js

---
+++ datosStyleBus.js
@@ -0,0 +1,67 @@
+// Formato Fecha yyyy/mm/dd
+
+let excursiones = [
+    {
+        'ubicacion': 'Misiones',
+        'descripcion': 'Excursion a la selva misionera',
+        'fecha': '2023-02-11'
+    },
+    {
+        'ubicacion': 'Misiones',
+        'descripcion': 'Recorrido por el Parque Nacional Iguazu',
+        'fecha': '2023-02-11'
+    },
+    {
+        'ubicacion': 'Mendoza',
+        'descripcion': 'Recorrido a viñedos mendocinos',
+        'fecha': '2023-06-25'
+    },
+    {
+        'ubicacion': 'Cordoba',
+        'descripcion': 'Recorrido en Villa General Belgrano',
+        'fecha': '2022-12-15'
+    },
+    {
+        'ubicacion': 'Tierra del fuego',
+        'descripcion': 'Excursion a Parque Nacional Tierra del Fuego',
+        'fecha': '2022-11-28'
+    }
+]
+
+let pasajesIda = [
+    {
+        'origen': 'Buenos Aires',
+        'destino': 'Jujuy',
+        'descripcion': 'Excursion a Tilcara',
+        'fecha': '2022-12-12',
+        'cantidadPasajeros': 2
+    },
+    {
+        'origen': 'Buenos Aires',
+        'destino': 'Chubut',
+        'descripcion': 'Recorrido por el Parque Nacional Iguazu',
+        'fecha': '2023-02-11',
+        'cantidadPasajeros': 4
+    },
+    {
+        'origen': 'Buenos Aires',
+        'destino': 'Neuquen',
+        'descripcion': 'Recorrido a viñedos mendocinos',
+        'fecha': '2023-06-25',
+        'cantidadPasajeros': 3
+    },
+    {
+        'origen': 'Buenos Aires',
+        'destino': 'La Pampa',
+        'descripcion': 'Recorrido en Villa General Belgrano',
+        'fecha': '2022-12-15',
+        'cantidadPasajeros': 2
+    },
+    {
+        'origen': 'Buenos Aires',
+        'destino': 'Tierra del fuego',
+        'descripcion': 'Excursion a Parque Nacional Tierra del Fuego',
+        'fecha': '2022-11-28',
+        'cantidadPasajeros': 1
+    }
+]
